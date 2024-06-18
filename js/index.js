8c5b2133e07645bb914092ce3c8970fc5c8b11eb
Danilo Montecel <daniloomarmontecel@gmail.com>
2024-06-18 09:04:27 -0500

    Restaurndo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 ///--Modo osuro--///
 
-const iconModo=document.querySelector(".iconSun");
+/*const iconModo=document.querySelector(".iconSun");
 iconModo.addEventListener("click",()=>{
     if(document.body.classList.toggle("light"))return(iconModo.textContent="🌑");
     iconModo.textContent="☀️"
 })
-
+*/
 //------///
 btnOpen.onclick=function(){
     nave.classList.toggle('show-menu')
+  }
+
+  const toggle= document.getElementById('container-dark');
+  const body = document.querySelector('body');
+
+  toggle.onclick= function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active')
   }
